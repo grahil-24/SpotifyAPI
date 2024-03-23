8ed5e2002b2ce57f7864a8b864c165c3e0dfff94
rahil <rahilganatra@gmail.com>
2024-03-23 13:45:37 +0530

    Modified getUserData to fetch only playlists instead of both playlists and songs when logging in

--- controller/getUserData.js
+++ controller/getUserData.js
@@ -2,13 +2,13 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const loginController = require('./loginController');
 const spotifyApi = new SpotifyWebApi();
 
-async function getMyData(constraint) {
+async function getMyData() {
     try {
         const accessToken = loginController.getAccessToken();
         spotifyApi.setAccessToken(accessToken);
         const me = await spotifyApi.getMe();
         console.log(me);
-        const playlists = await getUserPlaylists(me.body.id, constraint);
+        const playlists = await getUserPlaylists(me.body.id);
         return { user: me.body, playlists };
     } catch (error) {
         console.error(error);
@@ -16,29 +16,13 @@ async function getMyData(constraint) {
     }
 }
 
-async function getUserPlaylists(userName, constraint) {
+async function getUserPlaylists(userId) {
     try {
         let playlists = [];
-        let offset = 0;
-        let limit = 50; // Set your desired limit here
-
-        while (true) {
-            const data = await spotifyApi.getUserPlaylists(userName, { limit, offset });
-            for (let playlist of data.body.items) {
-                if (constraint === "get_playlist") {
-                    playlists.push({ name: playlist.name, id: playlist.id });
-                } else {
-                    let tracks = await getPlaylistTracks(playlist.id);
-                    playlists.push({ name: playlist.name, id: playlist.id, tracks });
-                }
-            }
-            if (data.body.next) {
-                offset += limit;
-            } else {
-                break;
-            }
+        const data = await spotifyApi.getUserPlaylists(userId);
+        for (let playlist of data.body.items) {
+            playlists.push({ name: playlist.name, id: playlist.id });
         }
-
         return playlists;
     } catch (error) {
         console.error(error);
