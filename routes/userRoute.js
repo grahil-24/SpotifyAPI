4252d89c315e1f6003f5ed25eeb88fc5c01bc8aa
rahil <rahilganatra@gmail.com>
2024-03-25 15:58:40 +0530

    added route for removing duplicates

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const viewController = require('./../controller/viewController');
 const loginController = require('./../controller/loginController');
 const userRouter = express.Router();
-const {getPlaylistTracks, getDuplicates} = require('../controller/dataController');
+const {getPlaylistTracks, getDuplicates, removeDuplicates} = require('../controller/dataController');
 
 userRouter.get('/', viewController.getLoginPage);
 userRouter.get('/login', loginController.login);
@@ -10,5 +10,6 @@ userRouter.get('/callback', loginController.initializeSpotifyAuth);
 userRouter.get('/home', viewController.getHomepage);
 
 userRouter.get('/home/getSongs', getDuplicates);
+userRouter.post('/home/removeDuplicates', removeDuplicates)
 
 module.exports = userRouter;    
