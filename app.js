3c9d5efc24d76baa65343c82f21d4c289e55928b
rahil <rahilganatra@gmail.com>
2024-03-25 15:58:23 +0530

    installed body-parser

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const app = express();
 const userRouter = require('./routes/userRoute');
 const path = require('path');
+const bodyParser = require('body-parser');
 
-app.use('/', userRouter);
-
+const app = express();
+app.use(bodyParser.json());
 // setting pug as the template engine
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+
+
+app.use('/', userRouter);
 
 module.exports = app;
